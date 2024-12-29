594dae0d44846f979942979115efd9502dc84406
Irene Riportella <riportellairene@gmail.com>
2024-12-29 21:43:08 +0100

    feat:✨ Exercise 4: Create ToastProvider component

---
+++ src/components/ToastProvider/ToastProvider.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export const ToastContext = React.createContext();
+
+function ToastProvider({ children }) {
+  const [toasts, setToasts] = React.useState([
+    {
+      id: crypto.randomUUID(),
+      message: "It works!",
+      variant: "success",
+    },
+    {
+      id: crypto.randomUUID(),
+      message: "Please check your username!",
+      variant: "warning",
+    },
+  ]);
+
+  function createToast(message, variant) {
+    const nextToasts = [
+      ...toasts,
+      {
+        id: crypto.randomUUID(),
+        message,
+        variant,
+      },
+    ];
+
+    setToasts(nextToasts);
+  }
+
+  function dismissToast(id) {
+    const nextToasts = toasts.filter((toast) => {
+      return toast.id !== id;
+    });
+
+    setToasts(nextToasts);
+  }
+
+  return (
+    <ToastContext.Provider value={{ toasts, createToast, dismissToast }}>
+      {children}
+    </ToastContext.Provider>
+  );
+}
+
+export default ToastProvider;
